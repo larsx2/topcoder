4c86bf48c487b5bd4ae9ec3c0c98e9e4505c958e
Eduardo Urias <windkaiser@gmail.com>
2015-03-05 14:30:43 -0700

    added: DoubleLetter problem

---
+++ div1/level1/DoubleLetter.py
@@ -0,0 +1,43 @@
+class DoubleLetter(object):
+
+    def ableToSolve(self, s):
+        if len(s) % 2 != 0:
+            return 'Impossible'
+
+        s = list(s)
+        
+        def cmp(s, j):
+            return s[j:j+2]
+
+        found = True 
+        while found:
+            found = False
+
+            for i in range(len(s)-1):
+                if s[i] == s[i+1]:
+                    found = True
+                    del s[i:i+2]
+                    break
+
+        if len(s):
+        	return 'Impossible'
+
+        return 'Possible'
+
+
+if __name__ == '__main__':
+    
+    double = DoubleLetter()
+
+    params = [
+        'aabccb',
+        'aabccbb',
+        'abcddcba',
+        'abab',
+        'aaaaaaaaaa',
+        'aababbabbaba',
+        'zzxzxxzxxzzx'
+    ]
+
+    for param in params:
+        print '"{}" = {}'.format(param, double.ableToSolve(param))
